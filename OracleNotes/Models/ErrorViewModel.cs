a5954c706e49f98986beb3cda4f759759ec9c27a
Michał B <cyberwolf_96@outlook.com>
2019-01-09 12:29:04 +0100

    Created new project.

---
+++ OracleNotes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OracleNotes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
